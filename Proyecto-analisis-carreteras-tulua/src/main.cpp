dd1892d8f7c7f5fe81b5a9d5bc9dca3e4565b372
LeyerOrdonez <leyer.ordonez01@uceva.edu.co>
2024-11-12 19:22:18 -0500

    (fix): se reparó los problemas en la conexion modulo gps

--- Proyecto-analisis-carreteras-tulua/src/main.cpp
+++ Proyecto-analisis-carreteras-tulua/src/main.cpp
@@ -4,9 +4,8 @@
 #include <Adafruit_SSD1306.h>    // Para la pantalla OLED
 #include <Adafruit_GFX.h>        // gráfica para la pantalla OLED
 
-
-//#include <TinyGPSPlus.h>
-
+#include <TinyGPSPlus.h>         // para el modulo gps
+#include <SoftwareSerial.h>      // para ampliar los tx y rx en esp32
 
 // Definición de parámetros
 #define SCREEN_WIDTH 128    //tamaño de pantall ancho
@@ -17,7 +16,10 @@ int buttonPin2 = 17;  // activar switch 2 ronda
 int buttonPin3 = 5;   // para activar siwtch 3 ronda
 int buttonPin4 = 18;  //  para enviar los datos, aun no se sabe donde
 
-//TinyGPSPlus gps;
+SoftwareSerial ss(12, 13); // RX, TX (ajusta según tus conexiones)   etan inversos
+TinyGPSPlus gps;
+
+
 
 //datos para detectar aceleracion brusca
 const float accelerationThreshold = 2.5;  // Threshold para detectar aceleración brusca en m/s²
@@ -39,7 +41,8 @@ void setup() {
     pinMode(buttonPin4, INPUT_PULLDOWN); //subir datos
 
     // Inicializar comunicación serial para depuración
-    Serial.begin(115200);
+    Serial.begin(115200);  
+    ss.begin(9600);
 
     // Inicializar el MPU6050
     if (!mpu.begin()) {
@@ -110,16 +113,43 @@ void leer_datos(int buttonPin){
     int movimientoBrusco = calcularEscalaDeMovimiento(accelMagnitude, gyroMagnitude);
 
     // Imprime la escala de 0 a 10
-    display.setCursor(0,0);
-    display.print("Perturbacion: ");
+    display.println("Perturbacion: ");
     display.println(movimientoBrusco);
 
     Serial.print(movimientoBrusco);
+    
+
+    float latitude = gps.location.lat();
+    float longitude = gps.location.lng();
+
+    Serial.print("Latitud: ");
+    Serial.println(latitude, 6);
+    Serial.print("Longitud: ");
+    Serial.println(longitude, 6);
+    Serial.print("Número de satélites: ");
+    Serial.println(gps.satellites.value());
+    Serial.print("HDOP: ");
+    Serial.println(gps.hdop.value());
+    Serial.print("Velocidad (km/h): ");
+    Serial.println(gps.speed.kmph());
+    Serial.println("-------------------------");
+
+    // Reiniciar el contador de tiempo
+    display.setCursor(0,0);
+    display.print("latitud: ");
+    display.println(latitude,6);
+    display.setCursor(0,10);
+    display.print("longitud: ");
+    display.println(longitude,6);
+    display.setCursor(0,20);
+    display.print("Nro de satelites: ");
+    display.println(gps.satellites.value());
     display.display();
 
-    delay(100);
+ 
     display.clearDisplay();
     delay(100);
+
     }
 
     display.clearDisplay();
@@ -133,14 +163,14 @@ void leer_datos(int buttonPin){
 void ronda1(){
     display.clearDisplay();
     display.setCursor(0, 0);
-    display.print("Iniciando Ronda 1 en...");
-    display.print(" 3");
+    display.println("Iniciando Ronda 1 en.. ");
+    display.println(" 3");
     display.display();
     delay(1000);
-    display.print(" 2");
+    display.println(" 2");
     display.display();
     delay(1000);
-    display.print(" 1");
+    display.println(" 1");
     display.display();
     delay(1000);
 
@@ -151,13 +181,13 @@ void ronda2(){
     display.clearDisplay();
     display.setCursor(0, 0);
     display.print("Iniciando Ronda 2 en...");
-    display.print(" 3");
+    display.println(" 3");
     display.display();
     delay(1000);
-    display.print(" 2");
+    display.println(" 2");
     display.display();
     delay(1000);
-    display.print(" 1");
+    display.println(" 1");
     display.display();
     delay(1000);
 
@@ -168,19 +198,18 @@ void ronda3(){
     display.clearDisplay();
     display.setCursor(0, 0);
     display.print("Iniciando Ronda 3 en...");
-    display.print(" 3");
+    display.println(" 3");
     display.display();
     delay(1000);
-    display.print(" 2");
+    display.println(" 2");
     display.display();
     delay(1000);
-    display.print(" 1");
+    display.println(" 1");
     display.display();
     delay(1000);
 
     leer_datos(buttonPin3);
 }
-
 
 void loop() {
   
@@ -206,7 +235,7 @@ void loop() {
     delay(1000);
     }
     
-     }else{
+    }else{
     display.clearDisplay();
     display.setCursor(0, 0);
     display.print("-----En espera-----");
@@ -218,7 +247,7 @@ void loop() {
     display.print("switch 4/subir data"); 
     display.display();
     delay(100);
-     }
+    }
      
 
 }
