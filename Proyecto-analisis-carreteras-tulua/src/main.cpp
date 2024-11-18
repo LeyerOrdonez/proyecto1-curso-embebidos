ba78c37eef0f181eba7de0e8769f10db84e6d4a2
LeyerOrdonez <leyer.ordonez01@uceva.edu.co>
2024-11-18 10:42:28 -0500

    (feature): envio de datos con json desde la esp32 activo

--- Proyecto-analisis-carreteras-tulua/src/main.cpp
+++ Proyecto-analisis-carreteras-tulua/src/main.cpp
@@ -2,12 +2,10 @@
 #include <Wire.h>                //i2c protocol
 #include <Adafruit_MPU6050.h>    // Para el acelerómetro MPU6050
 #include <Adafruit_SSD1306.h>    // Para la pantalla OLED
-#include <Adafruit_GFX.h>        // gráfica para la pantalla OLED
-
 #include <TinyGPSPlus.h>         // para el modulo gps
 #include <SoftwareSerial.h>      // para ampliar los tx y rx en esp32
 
-// Definición de parámetros
+// Definición de parámetros generales
 #define SCREEN_WIDTH 128    //tamaño de pantall ancho
 #define SCREEN_HEIGHT 64    //taaño de pantalla alto
 #define OLED_RESET    -1    // -1 oled = no reset
@@ -16,21 +14,19 @@ int buttonPin2 = 17;  // activar switch 2 ronda
 int buttonPin3 = 5;   // para activar siwtch 3 ronda
 int buttonPin4 = 18;  //  para enviar los datos, aun no se sabe donde
 
-SoftwareSerial ss(12, 13); // RX, TX (ajusta según tus conexiones)   etan inversos
-TinyGPSPlus gps;
-
+String globalArray[20];
+int i = 0;
 
 
 //datos para detectar aceleracion brusca
-const float accelerationThreshold = 2.5;  // Threshold para detectar aceleración brusca en m/s²
-const float gyroThreshold = 50;           // Threshold para giros bruscos en grados por segundo
-const float maxGyroAngle = 60.0;          // Máximo ángulo permitido antes de llegar a 10
+const float accelerationThreshold = 9.8;  // Threshold para detectar aceleración brusca en m/s²
 
 
-// Inicializa el MPU6050 y la pantalla OLED
+// Inicializa el MPU6050 y pantalla, gps y serial auxiliar
+SoftwareSerial ss(12, 13);                // RX, TX | amar-narj
+TinyGPSPlus gps;                          // creo la instancia del gps
 Adafruit_MPU6050 mpu;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 
 
 void setup() {
@@ -41,48 +37,44 @@ void setup() {
     pinMode(buttonPin4, INPUT_PULLDOWN); //subir datos
 
     // Inicializar comunicación serial para depuración
-    Serial.begin(115200);  
+    Serial.begin(115200);   // ---------delete on production---------- 
     ss.begin(9600);
-
-    // Inicializar el MPU6050
-    if (!mpu.begin()) {
-        Serial.println("Error al inicializar el MPU6050.");
-        while (1); // Bloquea el código si no se inicializa (peligroso)
-    }
-    Serial.println("MPU6050 inicializado correctamente.");
-
 
     // Inicializar la pantalla OLED
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Dirección 0x3C para la pantalla OLED
-        Serial.println("Error al inicializar la pantalla OLED.");
+        display.println("Error OLED.       CODIGO BLOQUEADO");
         while (1); // Bloquea el código si no se inicializa (peligroso)
     }
+
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
+    display.println("- OLED Listo!\n");
+
+    // Inicializar el MPU6050
+    if (!mpu.begin()) {
+        display.println("Error MPU6050.     CODIGO BLOQUEADO");
+        while (1); // Bloquea el código si no se inicializa (peligroso)
+    }
+    display.println("- MPU6050 listo!");
+
     display.display();
-
-
-    
-    
+    delay(3000);
+   
+ 
 }
 
-int calcularEscalaDeMovimiento(float accelMagnitude, float gyroMagnitude) {
+int calcularEscalaDeMovimiento(float accelMagnitude) {
     int escala = 0;
 
     // Si la aceleración es cercana a la gravedad (reposo)
-    if (fabs(accelMagnitude - 9.8) < 0.2 && gyroMagnitude < 1) {
+    if (fabs(accelMagnitude - 9.8) < 0.3 ) {
         return 0;  // Sin perturbación
     }
 
     // Aceleración brusca
     if (accelMagnitude > accelerationThreshold) {
         escala += map(accelMagnitude, accelerationThreshold, 15.0, 1, 5);  // Ajustar los límites según sea necesario
-    }
-
-    // Giros bruscos (medidos en grados por segundo)
-    if (gyroMagnitude > gyroThreshold) {
-        escala += map(gyroMagnitude, gyroThreshold, 500.0, 1, 5);  // Ajustar los límites
     }
 
     // Limita la escala a un máximo de 10
@@ -93,60 +85,63 @@ int calcularEscalaDeMovimiento(float accelMagnitude, float gyroMagnitude) {
     return escala;
 }
 
-void leer_datos(int buttonPin){
+void leer_datos(int buttonPin, int ronda){
     
     while(digitalRead(buttonPin) == HIGH){
 
-    int pantalla = 0;
     // Leer aceleración y giroscopio.
-
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
-
-    // Calcula la magnitud del vector de aceleración
-    float accelMagnitude = sqrt(sq(a.acceleration.x) + sq(a.acceleration.y) + sq(a.acceleration.z));
-    
-    // Calcula la magnitud del vector de giro
-    float gyroMagnitude = sqrt(sq(g.gyro.x) + sq(g.gyro.y) + sq(g.gyro.z)) * 57.2958;  // Convertir a grados/s
-
-    // Determina la escala de perturbación
-    int movimientoBrusco = calcularEscalaDeMovimiento(accelMagnitude, gyroMagnitude);
-
-    // Imprime la escala de 0 a 10
-    display.println("Perturbacion: ");
-    display.println(movimientoBrusco);
-
-    Serial.print(movimientoBrusco);
-    
+    // Leer datos del GPS
+    while (ss.available() > 0)
+    gps.encode(ss.read());
 
     float latitude = gps.location.lat();
     float longitude = gps.location.lng();
 
-    Serial.print("Latitud: ");
-    Serial.println(latitude, 6);
-    Serial.print("Longitud: ");
-    Serial.println(longitude, 6);
-    Serial.print("Número de satélites: ");
-    Serial.println(gps.satellites.value());
-    Serial.print("HDOP: ");
-    Serial.println(gps.hdop.value());
-    Serial.print("Velocidad (km/h): ");
-    Serial.println(gps.speed.kmph());
-    Serial.println("-------------------------");
+    // Calcula la magnitud del vector de aceleración
+    float accelMagnitude = sqrt(sq(a.acceleration.x) + sq(a.acceleration.y) + sq(a.acceleration.z));
 
-    // Reiniciar el contador de tiempo
+    // Determina la escala de perturbación
+    int movimientoBrusco = calcularEscalaDeMovimiento(accelMagnitude);
+
+
+    if(movimientoBrusco > 5){
+
+        if(i !=20){
+        globalArray[i] = "{";
+        globalArray[i] += "\"lat\": " + String(latitude, 6) + ", ";
+        globalArray[i] += "\"long\": " + String(longitude, 6) + ", ";
+        globalArray[i] += "\"lvl\": " + String(movimientoBrusco) + ", ";
+        globalArray[i] += "\"rnd\": \"" + String(ronda) + "\"";
+        globalArray[i] += "}";
+        }
+
+    i++;
+    }
+
+
+    // Imprime datos
     display.setCursor(0,0);
+    display.print("Perturbacion: ");
+    display.print(movimientoBrusco);
+    display.setCursor(0,10);
     display.print("latitud: ");
     display.println(latitude,6);
-    display.setCursor(0,10);
+    display.setCursor(0,20);
     display.print("longitud: ");
     display.println(longitude,6);
-    display.setCursor(0,20);
+    display.setCursor(0,30);
     display.print("Nro de satelites: ");
     display.println(gps.satellites.value());
-    display.display();
+    display.setCursor(0,40);
+    display.print("Velocidad (km/h):");
+    display.println(gps.speed.kmph());
 
- 
+    display.setCursor(0,50);
+    display.print("puntos restan:");
+    display.println(20-i);
+    display.display();
     display.clearDisplay();
     delay(100);
 
@@ -163,7 +158,7 @@ void leer_datos(int buttonPin){
 void ronda1(){
     display.clearDisplay();
     display.setCursor(0, 0);
-    display.println("Iniciando Ronda 1 en.. ");
+    display.println("Iniciando Ronda 1 en ");
     display.println(" 3");
     display.display();
     delay(1000);
@@ -174,13 +169,13 @@ void ronda1(){
     display.display();
     delay(1000);
 
-    leer_datos(buttonPin1);
+    leer_datos(buttonPin1, 1);
 }
 
 void ronda2(){
     display.clearDisplay();
     display.setCursor(0, 0);
-    display.print("Iniciando Ronda 2 en...");
+    display.println("Iniciando Ronda 2 en");
     display.println(" 3");
     display.display();
     delay(1000);
@@ -191,13 +186,13 @@ void ronda2(){
     display.display();
     delay(1000);
 
-    leer_datos(buttonPin2);
+    leer_datos(buttonPin2, 2);
 }
 
 void ronda3(){
     display.clearDisplay();
     display.setCursor(0, 0);
-    display.print("Iniciando Ronda 3 en...");
+    display.println("Iniciando Ronda 3 en");
     display.println(" 3");
     display.display();
     delay(1000);
@@ -208,7 +203,7 @@ void ronda3(){
     display.display();
     delay(1000);
 
-    leer_datos(buttonPin3);
+    leer_datos(buttonPin3, 3);
 }
 
 void loop() {
@@ -228,12 +223,21 @@ void loop() {
     display.setCursor(0, 0);
     display.clearDisplay();
     display.print("subiendo datos >");
-    for (int i = 0; i < 10; i++)
+    for (int x = 0; x < 10; x++)
     {
     display.print(">>");
     display.display();
     delay(1000);
     }
+
+    for (int y = 0; y < 10; y++) {
+    Serial.println(globalArray[y]);  // Imprime cada nombre en el monitor serial
+    }
+    i=0;
+
+     display.print("listo, datos restablecidos");
+    display.display();
+    delay(1000);
     
     }else{
     display.clearDisplay();
@@ -242,8 +246,12 @@ void loop() {
     display.setCursor(0, 10);
     display.print("-----iniciar?-----");
     display.setCursor(0, 20);
-    display.print("switch 1-3/ronda 1-3"); 
-    display.setCursor(0, 30);
+    display.print("switch 1/ronda 1/der"); 
+     display.setCursor(0, 30);
+    display.print("switch 2/ronda 2/cent"); 
+     display.setCursor(0, 40);
+    display.print("switch 3/ronda 3/izq"); 
+    display.setCursor(0, 50);
     display.print("switch 4/subir data"); 
     display.display();
     delay(100);
